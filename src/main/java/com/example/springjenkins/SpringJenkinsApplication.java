3fbc9639ecf664ddb3528d2a725b015f2a2547d4
varsha40 <varshac.1908@gmail.com>
2023-03-15 18:20:50 +0530

    Minor changes.

--- src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/example/springjenkins/SpringJenkinsApplication.java
@@ -1,11 +1,11 @@
 package com.example.springjenkins;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
